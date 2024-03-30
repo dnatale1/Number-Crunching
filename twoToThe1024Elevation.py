750671b6cec6e5dc5410be5834d19ec498ee9b97
dnatale1 <113316354+dnatale1@users.noreply.github.com>
2024-03-30 11:33:24 -0400

    Add files via upload

--- twoToThe1024Elevation.py
+++ twoToThe1024Elevation.py
@@ -15,11 +15,11 @@ import math
 
 t1 = datetime.datetime.now()
 
-b = (2 ** 1024) ** 4194304
+b = (2 ** 1024) ** 16777216
 
 c = math.trunc(math.log10(b) + 1)
 
-print ('(2 ^ 1024) ^ 4194304 has ', c , ' digits')
+print ('(2 ^ 1024) ^ 16777216 has ', c , ' digits')
 
 print ('\n\nThe first 10 digits are: ', b // 10**(c - 10))
 
@@ -30,11 +30,11 @@ t2 = datetime.datetime.now()
 print (t2 - t1)
 
 
-# (2 ^ 1024) ^ 8388608 has  2585827973  digits
+# (2 ^ 1024) ^ 16777216 has  5171655946  digits
 
 
-# The first 10 digits are:  9630350133
+# The first 10 digits are:  9274364370
 
 
-# The last 10 digits are:  1 3 3 6 0 1 6 8 9 6
-# 8:44:15.728184
+# The last 10 digits are:  6 3 9 7 4 7 4 8 1 6
+# 1 day, 19:26:49.749055
