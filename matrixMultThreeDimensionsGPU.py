5ea35f9b214257e1e943ec017fed97aabdeaf18c
dnatale1 <113316354+dnatale1@users.noreply.github.com>
2026-09-16 13:55:27 -0400

    Add files via upload

--- matrixMultThreeDimensionsGPU.py
+++ matrixMultThreeDimensionsGPU.py
@@ -7,7 +7,7 @@
 #
 # I used PyTorch 2.6 and Python 3.10.
 #
-# The last entry in the PYMATRIXRESULT3D file is: 37,804,415,147,013,000,000,000,000.
+# The last entry in the PYMATRIXRESULT3D file is: 50,704,469,048,159,710,000,000,000.
 #
 # Ran on Jarvislabs.ai GPU Cloud Platform in India for 2 minutes.
 
@@ -28,23 +28,23 @@ t1 = datetime.datetime.now()
 sum = 0
 
 # 1. Generate Tensor T
-T = torch.zeros((2337, 2337, 2337), dtype=torch.int64)
-elements_A = 2333 * 2334 * 2335
+T = torch.zeros((2437, 2437, 2437), dtype=torch.int64)
+elements_A = 2433 * 2434 * 2435
 
 # Create a flattened sequence [10, 20, 30, ...] and reshape it to fit the slice
-seq_A = torch.arange(10, elements_A * 10 + 1, 10, dtype=torch.int64).view(2333, 2334, 2335)
-T[:2333, :2334, :2335] = seq_A
+seq_A = torch.arange(10, elements_A * 10 + 1, 10, dtype=torch.int64).view(2433, 2434, 2435)
+T[:2433, :2434, :2435] = seq_A
 
 
 sum = 0
 
 # 2. Generate Tensor U
-U = torch.zeros((2337, 2337, 2337), dtype=torch.int64)
-elements_B = 2335 * 2336 * 2337
+U = torch.zeros((2437, 2437, 2437), dtype=torch.int64)
+elements_B = 2435 * 2436 * 2437
 
 # Create the sequence for B and reshape it to fit the slice
-seq_B = torch.arange(10, elements_B * 10 + 1, 10, dtype=torch.int64).view(2335, 2336, 2337)
-U[:2335, :2336, :2337] = seq_B
+seq_B = torch.arange(10, elements_B * 10 + 1, 10, dtype=torch.int64).view(2435, 2436, 2437)
+U[:2435, :2436, :2437] = seq_B
 
 
 outFile1 = open('PYMATRIX13D', 'w')
